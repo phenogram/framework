d00b98b4d30f4df81e8dc101190422410740fbb4
Nikolai Shangin <shanginn@gmail.com>
2025-05-06 21:04:11 +0500

    Bump version to 6.0.1 and add a new method for opening files in the BufferedFile class, including exception handling for filesystem errors.

--- src/Type/BufferedFile.php
+++ src/Type/BufferedFile.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Phenogram\Framework\Type;
 
 use Amp\ByteStream\ReadableBuffer;
+use Amp\File\FilesystemException;
 
 class BufferedFile implements ReadableStreamFileInterface
 {
@@ -18,5 +19,23 @@ class BufferedFile implements ReadableStreamFileInterface
         private readonly string $content,
         public readonly string $filename,
     ) {
+    }
+
+    /**
+     * @throws FilesystemException
+     */
+    public static function openFile(string $path): self
+    {
+        $file = \Amp\File\openFile($path, 'r');
+        $content = '';
+
+        while (null !== $chunk = $file->read()) {
+            $content .= $chunk;
+        }
+
+        return new self(
+            content: $content,
+            filename: basename($path),
+        );
     }
 }
